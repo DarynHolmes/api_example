f32af3c750fdbaa0d364c5299593a4780afe5db4
Daryn Holmes <daryn.holmes@net-a-porter.com>
2015-08-10 17:00:26 +0100

    Added module

--- project_teams/server/server.rb
+++ project_teams/server/server.rb
@@ -1,5 +1,6 @@
-require 'sinatra'
 require 'json'
+require 'sinatra/base'
+
 
 Dir[File.dirname(__FILE__) + '/src/models/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/src/api/*.rb'].each {|file| require file }
--- project_teams/server/src/api/teams_api.rb
+++ project_teams/server/src/api/teams_api.rb
@@ -1,22 +1,73 @@
-$id_counter = 1
-$teams = []
-
-get '/teams' do
-	list = []
-	$teams.each do | team | 
-		team_hash = {}
-		team_hash[:id] = team.id
-		team_hash[:name] = team.name
-		list << team_hash		
+class Server < Sinatra::Base
+	
+	configure do
+	    set :id_counter, 1
+	    set :teams, []
+	    set :my_config_property, 'hello world'
 	end
-	list.to_json
+
+  	def teams
+  		$teams ||= [] 
+  	end
+
+  	def id_counter
+  		$id_counter ||= 1
+  	end
+
+  	get '/' do
+    	"#{settings.my_config_property}"
+  	end
+
+	get '/teams' do
+		list = []
+		teams.each do | team | 
+			team_hash = {}
+			team_hash[:id] = id_counter
+			team_hash[:name] = team.name
+			list << team_hash		
+		end
+		list.to_json
+	end
+
+	post '/teams' do
+		team = Team.new
+		team.id = id_counter
+		team.name = params['name']
+		settings.teams << team
+		id_counter += 1
+		status 201
+	end
+
 end
 
-post '/teams' do
-	team = Team.new
-	team.id = $id_counter
-	team.name = params['name']
-	$teams << team
-	$id_counter += 1
-	status 201
-end
+Server.run!
+
+# See below for non-module style server
+# in server.rb use
+# require 'sinatra'
+# and not 
+#require 'sinatra/base'
+
+# $id_counter = 1
+# $teams = []
+
+# get '/teams' do
+# 	list = []
+# 	$teams.each do | team | 
+# 		team_hash = {}
+# 		team_hash[:id] = team.id
+# 		team_hash[:name] = team.name
+# 		list << team_hash		
+# 	end
+# 	list.to_json
+# end
+
+# post '/teams' do
+# 	team = Team.new
+# 	team.id = $id_counter
+# 	team.name = params['name']
+# 	$teams << team
+# 	$id_counter += 1
+# 	status 201
+# end
+
